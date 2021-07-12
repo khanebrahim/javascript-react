bba1eee719cd0ab13154d4c6f108ebd5f3b3fcf8
BengalDroid <bengaldroid@gmail.com>
2021-07-13 01:45:20 +0600

    if else

---
+++ conditional1.js
@@ -0,0 +1,8 @@
+var isJavascript = true;
+var jobSavingTaka = 20000;
+if(isJavascript==true && jobSavingTaka>200000){
+    console.log("you will married 2023");
+} 
+else{
+    console.log("you will married 2025");
+}
