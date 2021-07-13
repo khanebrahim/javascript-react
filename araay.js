e8b4ceb2d64cc6edca651c00062be0ba47d6da47
BengalDroid <bengaldroid@gmail.com>
2021-07-13 16:56:09 +0600

    push array number

--- araay.js
+++ araay.js
@@ -1,5 +1,7 @@
 var friendsAge=[15,17,14,98,67]
 friendsAge[1]= 28;
 var position =friendsAge.indexOf(14);
-// console.log(friendsAge);
+console.log(friendsAge);
 console.log(position);
+friendsAge.push(23);
+console.log(friendsAge);
