81e1e9ce8d8e4c48a0a486b5ba4d94160000aac8
BengalDroid <bengaldroid@gmail.com>
2021-07-15 00:38:08 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,21 @@
+var num =1000;
+switch(num){
+    case 1000:
+        console.log('I am a badshah');
+        
+        // break;
+    case 100:
+        console.log('I am a baizid');
+        // break;
+    case 200:
+        console.log('I am a kali');
+        // break;
+    case 300:
+    case 400:
+    case 500:
+        console.log('he is a rich man');
+        // break;
+        default:
+            console.log('I dont know who you are');
+
+}
