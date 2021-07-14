b627f0d0e43ef98dbaae2a2f503343cc72435e34
BengalDroid <bengaldroid@gmail.com>
2021-07-15 02:35:26 +0600

    sum1

--- function2.js
+++ function2.js
@@ -4,7 +4,18 @@ function doubleNumber(num){
 }
 var first = doubleNumber(56);
 var second = doubleNumber(34);
-console.log(first,second);
+var total = first+second;
+
+function add(num1,num2){
+
+    var sum=num1+num2;
+    return sum;
+}
+
+var sum1 = add(15,45);
+console.log(sum1);
+
+
 
 
 
