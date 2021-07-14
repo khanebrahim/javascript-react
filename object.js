c05525f365d7e566233bfabd44564e56d90c7be3
BengalDroid <bengaldroid@gmail.com>
2021-07-15 03:20:43 +0600

    adding new index

--- object.js
+++ object.js
@@ -1,6 +1,7 @@
 var baizid={id:171 ,phoneNumber:19123 , name:'mocha'};
 var habiba= {id:172,phoneNumber:19334, name:'company'};
-var abba =baizid['phoneNumber','name'];
+var abba =baizid['phoneNumber'];
+baizid.webdev="react";
 
 console.log(abba);
 console.log(baizid);
