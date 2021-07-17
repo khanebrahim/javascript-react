c8183072b231730d8e526e6a59737598f894e3cf
BengalDroid <bengaldroid@gmail.com>
2021-07-18 01:22:48 +0600

    function

--- bonus.js
+++ bonus.js
@@ -4,3 +4,15 @@ for(var i =0; i<marks.length; i++){
 
     console.log(num);
 }
+console.log(`habiba`);
+console.log(`abbu`);
+function add(number1,number2){
+    var total =number1+number2;
+    return total;
+}
+var result1 = add(23,34);
+console.log(result1);
+console.log(`not adding this time`);
+console.log(`not eating right now`);
+var result2 = add(234,76);
+console.log(result2);
